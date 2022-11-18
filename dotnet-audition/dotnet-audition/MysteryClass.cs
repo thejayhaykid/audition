716a9eeb20799def915cc2c3b02ab7306eb73c93
Nick Alt <altnp291@gmail.com>
2022-11-18 00:28:47 -0500

    update mystery prompt

--- dotnet-audition/dotnet-audition/MysteryClass.cs
+++ dotnet-audition/dotnet-audition/MysteryClass.cs
@@ -5,13 +5,13 @@ namespace dotnet_audition
      /**
      * Read the following code and:
      * a) Explain what the code is supposed to do.
-     * b) Write a test case(s) to prove it.
+     * b) Write test case(s) to prove it.
      * c) If there are errors, explain them & provide a solution.
      */
 
     public class MysteryClass
     {
-        public string Mystery(string num)
+        public string Mystery(string num) 
         {
             if (num == null) {
                 return "N/A";
@@ -20,16 +20,16 @@ namespace dotnet_audition
             var len = num.Length;
             var c = 0;
             var sb = new char[len];
-
-            for (var i = 0; i < len; i++)
+            
+            for (var i = 0; i < len; i++) 
             {
                 sb[c++] = num[i];
-                if ((len - 1 - i) % 3 == 0 && i != len - 1)
+                if ((len - 1 - i) % 3 == 0 && i != len - 1) 
                 {
                     sb[c++] = ',';
                 }
             }
-
+            
             return new string(sb);
         }
     }
